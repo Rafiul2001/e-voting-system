c67c3e9e52dac61b9e74e2da38c1cb5b09e0fc83
Shah Md. Rafiul Kadir <rafiul13062001@gmail.com>
2025-09-03 13:41:18 +0600

    Bug fixed in election contract

--- network/invoke-electioncc.sh
+++ network/invoke-electioncc.sh
@@ -15,6 +15,16 @@ export CHANNEL_NAME=votingchannel
 
 set_peer_env localhost:8051 "ElectionCommissionMSP" "${PWD}/organizations/peerOrganizations/electionCommission.example.com/peers/peer0.electionCommission.example.com/tls/ca.crt" "${PWD}/organizations/peerOrganizations/electionCommission.example.com/users/Admin@electionCommission.example.com/msp"
 
+# peer chaincode invoke \
+#     -o localhost:7050 \
+#     --ordererTLSHostnameOverride orderer.example.com \
+#     --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
+#     -C ${CHANNEL_NAME} \
+#     -n ${CC_NAME} \
+#     --peerAddresses localhost:8051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/electionCommission.example.com/peers/peer0.electionCommission.example.com/tls/ca.crt" \
+#     --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/districtCommission.example.com/peers/peer0.districtCommission.example.com/tls/ca.crt" \
+#     -c '{"function":"Initialize","Args":["ELPID002"]}'
+
 peer chaincode invoke \
     -o localhost:7050 \
     --ordererTLSHostnameOverride orderer.example.com \
@@ -23,7 +33,17 @@ peer chaincode invoke \
     -n ${CC_NAME} \
     --peerAddresses localhost:8051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/electionCommission.example.com/peers/peer0.electionCommission.example.com/tls/ca.crt" \
     --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/districtCommission.example.com/peers/peer0.districtCommission.example.com/tls/ca.crt" \
-    -c '{"function":"Initialize","Args":["ELPID005"]}'
+    -c '{"function":"StartElection","Args":["ELPID002"]}'
+
+# peer chaincode invoke \
+#     -o localhost:7050 \
+#     --ordererTLSHostnameOverride orderer.example.com \
+#     --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
+#     -C ${CHANNEL_NAME} \
+#     -n ${CC_NAME} \
+#     --peerAddresses localhost:8051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/electionCommission.example.com/peers/peer0.electionCommission.example.com/tls/ca.crt" \
+#     --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/districtCommission.example.com/peers/peer0.districtCommission.example.com/tls/ca.crt" \
+#     -c '{"function":"FinishElection","Args":["ELPID002"]}'
 
 # set_peer_env localhost:9051 "DistrictCommissionMSP" "${PWD}/organizations/peerOrganizations/districtCommission.example.com/peers/peer0.districtCommission.example.com/tls/ca.crt" "${PWD}/organizations/peerOrganizations/districtCommission.example.com/users/Admin@districtCommission.example.com/msp"
 
