29f144e8bcd586f6a091b56a72811f0ee834cdd5
Rafiul2001 <rafiul13062001@gmail.com>
2025-11-13 00:28:41 +0600

    permit router is initialized

--- operatorPanel/api/src/routes/permit_router.ts
+++ operatorPanel/api/src/routes/permit_router.ts
@@ -1,8 +1,72 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { verifyToken } from "../middlewares/verifyToken";
+import {
+  getPermit,
+  IssuePermit,
+  SpendPermit,
+} from "../networkConnection/voterPermitContractFunctions/voterPermitContractFuntions";
 
 const permitRouter = Router();
 
-// Machine can cast vote
+// Operator can issue permit
+permitRouter.post(
+  "/issue-permit",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { permitKey, voterId, operatorId, electionId } = req.body;
+    try {
+      const responseFromContractFunction = await IssuePermit(
+        permitKey,
+        voterId,
+        operatorId,
+        electionId
+      );
+
+      return res.json(responseFromContractFunction);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// Machine can spend permit
+permitRouter.post(
+  "/spend-permit",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { permitKey, electionId, machineId } = req.body;
+    try {
+      const responseFromContractFunction = await SpendPermit(
+        permitKey,
+        electionId,
+        machineId
+      );
+
+      return res.json(responseFromContractFunction);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// Operator can get permit
+permitRouter.get(
+  "/get-permit",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { voterId, electionId, operatorId } = req.body;
+    try {
+      const responseFromContractFunction = await getPermit(
+        voterId,
+        electionId,
+        operatorId
+      );
+
+      return res.json(responseFromContractFunction);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default permitRouter;
